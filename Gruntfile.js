258d9e0eca20ada7ba7c8f8f95c901035ce7b991
michbuett <michbuett@gmx.de>
2015-09-02 16:11:54 +0200

    Setup project

---
+++ Gruntfile.js
@@ -0,0 +1,111 @@
+module.exports = function (grunt) {
+    'use strict';
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        watch: {
+            all: {
+                files: ['Gruntfile.js', 'src/**/*.js', 'tests/**/*.js'],
+                tasks: ['test']
+            },
+        },
+
+        jshint: {
+            all: ['Gruntfile.js', 'src/**/*.js', 'tests/**/*.js'],
+            options: {
+                jshintrc: '.jshintrc',
+            }
+        },
+
+        jasmine_nodejs: {
+            all: {
+                specs: ['tests/**'],
+            },
+        },
+
+        browserify: {
+            dist: {
+                src: [
+                    'src/**/*.js',
+                ],
+                dest: 'dist/coquo-venenum.js',
+                options: {
+                    browserifyOptions: {
+                        debug: false,
+                        standalone: 'coquo-venenum',
+                    },
+                }
+            },
+        },
+
+        jasmine: {
+            dist: {
+                src: ['dist/coquo-venenum.js'],
+
+                options: {
+                    keepRunner: true,
+                    specs: 'tests/**/*.js',
+                },
+            },
+
+            coverage: {
+                src: ['src/**/*.js'],
+
+                options: {
+                    keepRunner: true,
+                    specs: 'tests/**/*.js',
+                    template: require('grunt-template-jasmine-istanbul'),
+                    templateOptions: {
+                        template: require('grunt-template-jasmine-nml'),
+                        templateOptions: {
+                            sourceRoot: '.grunt/grunt-contrib-jasmine/',
+                        },
+                        coverage: 'reports/coverage/coverage.json',
+                        report: 'reports/coverage',
+                        thresholds: {
+                            lines: 85,
+                            statements: 85,
+                            branches: 80,
+                            functions: 90
+                        },
+                    }
+                },
+            },
+        },
+
+        clean: {
+            dist: ['dist/*'],
+        },
+
+        uglify: {
+            dist: {
+                files: {
+                    'dist/coquo-venenum.min.js': ['dist/coquo-venenum.js']
+                }
+            },
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-browserify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-jasmine-nodejs');
+
+    grunt.registerTask('test', [
+        'jshint',
+        'jasmine_nodejs',
+        'jasmine:coverage',
+        'dist',
+        'jasmine:dist',
+    ]);
+
+    grunt.registerTask('dist', [
+        'clean',
+        'browserify:dist',
+        'uglify:dist'
+    ]);
+};
---
+++ tests/coquo.venenum.spec.js
@@ -0,0 +1,7 @@
+describe('coquo-venenum', function () {
+    'use strict';
+
+    it('can create brew-able potions', function () {
+        expect(true).toBeTruthy();
+    });
+});
